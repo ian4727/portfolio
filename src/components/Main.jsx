d803cb8e71dae381013587e958622622cc1b685e
elaine gervacio <elaine_gervacio@yahoo.com>
2024-08-17 17:14:10 +0800

    added main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,24 @@ import React from "react"
 
 const Main = () => {
     return (
-        <main className="container px-10 py-32 w-screen h-screen">
-            <div>
-                <h1 className="text-3xl">Hi! My name is <span className="text-[#f0bbd4] font-bold">Ian</span>.</h1>
-                <p className="text-xl">I'm a FrontEnd Web Developer from the Philippines.</p>
+        <main className="px-10 pt-48 h-screen flex flex-col justify-between">
+            <div className="flex items-center justify-between">
+                <div>
+                    <h1 className="text-3xl typing-effect">Hi! My name is <span className="text-[#f0bbd4] font-bold">Ian</span>.</h1>
+                    <p className="text-xl">I'm a Coding Instructor and FrontEnd Web Developer from the Philippines.</p>
+                </div>
+                <div>
+                    <img src="/src/assets/images/ian.png" className="z-40" alt="Animated photo" />
+                    {/* <img src="/src/assets/images/vector1.png" className="absolute top-20 z-10" alt="vector" /> */}
+                </div>
+                
             </div>
-            <img src="" alt="" />
+            <div className="flex justify-center mt-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 animate-bounce text-[#f0bbd4]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                </svg>
+            </div>
+
         </main>  
     )
 }
