f8426e4232369d9c4fa17c3cd7d67dea00b88dd1
elaine gervacio <elaine_gervacio@yahoo.com>
2024-08-21 18:34:29 +0800

    added blurred blots effect as background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,8 +5,8 @@ const Projects = () => {
     <section className="xl:w-screen xl:h-screen px-6 xl:px-32 py-60 xl:pt-80">
         <h1 className="text-4xl xl:text-6xl font-bold mb-32 text-center xl:text-left">Projects</h1>
         <div className="flex justify-center">
-            <ul className="grid xl:grid-cols-2 gap-10 w-[70%]">
-                <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
+            <ul className="grid xl:grid-cols-2 gap-10 w-[70%] relative">
+                <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 z-10">
                     <a href="https://frugalearth.pythonanywhere.com/">
                         <img src="/assets/images/frugalearth.png" alt="Frugal earth" className="h-[230px] xl:h-[600px]" />
                         <div className="p-6">
@@ -16,7 +16,7 @@ const Projects = () => {
                         </div>
                     </a>
                 </li>
-                <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
+                <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 z-10">
                     <iframe src="https://crashr.io/" className="w-full h-[210px] xl:h-[580px] p-4 rounded-xl" title="Project Preview">
                     </iframe>
                     <div className="px-6 py-10">
@@ -29,6 +29,8 @@ const Projects = () => {
                             
                         </div>
                 </li>
+
+                <div className="absolute w-96 h-56 rounded-full filter blur-[50px] -bottom-1 left-2 bg-pink z-0"></div>
             </ul>
         </div>
         
