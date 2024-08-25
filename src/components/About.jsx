ad7c6f433ad693ffa08150fd148c2a44b7ae2755
elaine gervacio <elaine_gervacio@yahoo.com>
2024-08-25 13:58:00 +0800

    adjusted aabot section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,10 @@ const About = () => {
             <h1 className="text-4xl xl:text-6xl font-bold mb-32 text-center xl:text-left">Get to know me</h1>
             <div className="text-md xl:text-3xl flex flex-col gap-10 xl:gap-20 justify-center">
                 <div className="transform transition-transform duration-300 hover:scale-110">
-                    <a href="/assets/files/gervacio-elaine.pdf" target="_blank" rel="noopener noreferrer">
-                        <div className="flex items-center">
-                            <img src="/assets/images/socials/resume.png" alt="Mobile" className="w-20 xl:w-24 ml-3 xl:ml-2 mr-7 xl:mr-4" />
-                            <span>Check my resume</span>
-                        </div>
-                    </a>
+                    <div className="flex items-center">
+                        <img src="/assets/images/socials/placeholder.png" alt="Mobile" className="w-20 transform transition-transform duration-300 hover:scale-101 ml-4 mr-6" />
+                        <span>Manila, Philippines</span>
+                    </div>
                 </div>
                 <div className="transform transition-transform duration-300 hover:scale-110">
                     <a href="mailto:edelhongervacio@gmail.com">
@@ -28,10 +26,12 @@ const About = () => {
                     </div>
                 </div>
                 <div className="transform transition-transform duration-300 hover:scale-110">
-                    <div className="flex items-center">
-                        <img src="/assets/images/socials/placeholder.png" alt="Mobile" className="w-20 transform transition-transform duration-300 hover:scale-101 ml-4 mr-6" />
-                        <span>Manila, Philippines</span>
-                    </div>
+                    <a href="/assets/files/gervacio-elaine.pdf" target="_blank" rel="noopener noreferrer">
+                        <div className="flex items-center">
+                            <img src="/assets/images/socials/resume.png" alt="Mobile" className="w-20 xl:w-24 ml-3 xl:ml-2 mr-7 xl:mr-4" />
+                            <span>Check my resume</span>
+                        </div>
+                    </a>
                 </div>
             </div>
         </section>
