d904fde62e9b55167236faec197a6a189c566200
elaine gervacio <elaine_gervacio@yahoo.com>
2024-08-25 13:55:40 +0800

    added freshlay project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <section className="py-96 pb-60">
         <h1 className="text-4xl xl:text-6xl font-bold mb-32 text-center xl:text-left px-6 xl:px-32 z-10">Projects</h1>
         <div className="flex justify-center pb-10">
-            <ul className="grid xl:grid-cols-2 gap-10 w-[70%] relative">
+            <ul className="flex flex-col gap-10 relative">
                 <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 z-10">
                     <a href="https://frugalearth.pythonanywhere.com/">
                         <div className="bg-[url('/assets/images/frugalearth.png')] bg-cover h-[400px] p-4 w-full">
@@ -20,19 +20,34 @@ const Projects = () => {
                     </a>
                 </li>
                 <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 z-10">
+                    <iframe src="https://ian4727.github.io/Project2/" className="w-full h-[400px] p-4 rounded-xl" title="Project Preview">
+                    </iframe>
+                    <div className="p-6">
+                        <h3 className="text-2xl font-bold mb-2">Freshlay</h3>
+                        <p className="mb-4">This is a prototype design I created while on Training at SAIB for an e-commerce business.</p>
+                        <p><span className="font-bold">Technologies Learned:</span> Blazor, Tailwind, MudBlazor</p>
+                        <div>
+                            <a href="https://ian4727.github.io/Project2/" className="text-pink hover:underline" target="_blank">View Project</a>
+                            <a href=""></a>
+                        </div>
+                        
+                    </div>
+                </li>
+                <li className="bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 z-10">
                     <iframe src="https://crashr.io/" className="w-full h-[400px] p-4 rounded-xl" title="Project Preview">
                     </iframe>
                     <div className="p-6">
-                            <h3 className="text-2xl font-bold mb-2">Crashr</h3>
-                            <p className="mb-4">Crashr is a Web3 Trading Platform.</p>
-                            <p><span className="font-bold">Technologies Learned:</span> Blazor, Tailwind, MudBlazor</p>
-                            <div>
-                                <a href="https://crashr.io/" className="text-pink hover:underline" target="_blank">View Project</a>
-                                <a href=""></a>
-                            </div>
-                            
+                        <h3 className="text-2xl font-bold mb-2">Crashr</h3>
+                        <p className="mb-4">Crashr is a Web3 Trading Platform.</p>
+                        <p><span className="font-bold">Technologies Learned:</span> Blazor, Tailwind, MudBlazor</p>
+                        <div>
+                            <a href="https://crashr.io/" className="text-pink hover:underline" target="_blank">View Project</a>
+                            <a href=""></a>
                         </div>
+                        
+                    </div>
                 </li>
+                
 
                 <div className="absolute w-96 h-56 rounded-full filter blur-[50px] -bottom-1 left-2 bg-pink z-0"></div>
             </ul>
